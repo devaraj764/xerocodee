66afcaec52bb7d7bec47b92fe9d8a2590a06f124
devaraj764 <devaraju.edgroom.com>
2023-09-23 23:27:53 +0530

    Edited passport.js

--- server/passport.js
+++ server/passport.js
@@ -6,10 +6,12 @@ var GoogleStrategy = require('passport-google-oauth20').Strategy;
 const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
 
+const URL = process.env.NODE_ENV === 'production' ? proocess.env.PRODUCTION_URL : '';
+
 passport.use(new GoogleStrategy({
     clientID: GOOGLE_CLIENT_ID,
     clientSecret: GOOGLE_CLIENT_SECRET,
-    callbackURL: "/api/auth/google/callback",
+    callbackURL: `${URL}/api/auth/google/callback`,
     passReqToCallback: true
 }, async (req, accessToken, refreshToken, profile, cb) => {
     const defaultUser = {
